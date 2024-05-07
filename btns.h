850c61573aa1163941a096c24adb882f12e97562
olehdzhufer <oleh.dzhufer.ir.2022@lpnu.ua>
2024-05-08 01:35:02 +0300

    upd_version_flag_for_read_from_button

--- btns.h
+++ btns.h
@@ -1,15 +1,17 @@
 #include "lcd.h"
 
 int buttonState = 0;
-int lastButtonState = LOW;  
-unsigned long lastDebounceTime = 0;  
 unsigned long debounceDelay = 100; 
 
-int lastButtonState2 = LOW;  
-unsigned long lastDebounceTime2 = 0; 
+struct Button{
+  uint16_t pin;
+  uint16_t lastDebTime ;
+  uint16_t lastButtState ;
+};
 
-int lastButtonState3 = LOW;  
-unsigned long lastDebounceTime3 = 0; 
+struct Button btn1 = {5, 0, 0};
+struct Button btn2 = {6, 0, 0};
+struct Button btn3 = {7, 0, 0};
 
 void buttons_setup(void)
 {
@@ -17,33 +19,44 @@ void buttons_setup(void)
     pinMode(BTN2, INPUT);
     pinMode(BTN3, INPUT);
 }
+
+void controlLEDAndLCD(bool isLedOn, uint16_t led*, char text*) {
+    if (isLedOn) {
+        digitalWrite(led*, LOW);
+        lcd.clear();  
+        lcd.setCursor(0, 0);
+        lcd.print(text*);
+    } else {
+        digitalWrite(led*, HIGH);
+        lcd.clear();                       
+        lcd.setCursor(0, 1);
+        lcd.print(text*);
+    }
+}
+
 void loop_buttons(){
 int reading = digitalRead(BTN1);  
 
-  if (millis() - lastDebounceTime > debounceDelay) {
-    lastDebounceTime = millis();
-    if (reading != lastButtonState) {
-      lastButtonState = reading;
+  if (millis() - btn1.lastDebTime > debounceDelay) {
+    btn1.lastDebTime = millis();
+    if (reading != btn1.lastButtState) {
+      btn1.lastButtState = reading;
       if (reading == HIGH) {
-        digitalWrite(LED1, LOW);
-        lcd.clear();  
-        lcd.setCursor(0, 0);
-        lcd.print("Green");
+        bool isLedOn = true;
+        controlLEDAndLCD(LED1, "Green on")
       } else {
-        digitalWrite(LED1, HIGH);
-        lcd.clear();                       
-        lcd.setCursor(0, 1);
-        lcd.print("Green off");  
+        bool isLedOn = false;
+        controlLEDAndLCD(LED1, "Green off") 
       }
     }
   }
 
   int reading_2 = digitalRead(BTN2);  
   
-  if (millis() - lastDebounceTime2 > debounceDelay) {
-    lastDebounceTime2 = millis();
-    if (reading_2 != lastButtonState2) {
-      lastButtonState2 = reading_2;
+  if (millis() - btn2.lastDebTime > debounceDelay) {
+    btn2.lastDebTime = millis();
+    if (reading_2 != btn2.lastButtState) {
+      btn2.lastButtState = reading_2;
       if (reading_2 == HIGH) {
         digitalWrite(LED2, LOW);
         lcd.clear();  
@@ -60,10 +73,10 @@ int reading = digitalRead(BTN1);
 
   int reading_3 = digitalRead(BTN3);  
   
-  if (millis() - lastDebounceTime2 > debounceDelay) {
-    lastDebounceTime3 = millis();
-    if (reading_3 != lastButtonState3) {
-      lastButtonState3 = reading_3;
+  if (millis() - btn3.lastDebTime > debounceDelay) {
+    btn3.lastDebTime = millis();
+    if (reading_3 != btn3.lastButtState) {
+      btn3.lastButtState = reading_3;
       if (reading_3 == HIGH) {
         digitalWrite(LED3, LOW);
         lcd.clear();  
