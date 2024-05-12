998a6503c98384087e47227e390da526515c86a1
olehdzhufer <oleh.dzhufer.ir.2022@lpnu.ua>
2024-05-13 01:27:12 +0300

    upgrade

--- lcd.h
+++ lcd.h
@@ -8,25 +8,11 @@
 #define LCD_ROWS 4
 #define LCD_COLS 20
 
-LiquidCrystal_I2C lcd2(,20,4);
+LiquidCrystal_I2C lcd(0x3F,20,4);
 
 void lcd_setup(){
   lcd.init();         
-  lcd.backlight();  
-
-  lcd2.init();         
-  lcd2.backlight();  
-  lcd2.setCursor(0,0);            
-  lcd2.print("Hello");
-  lcd2.setCursor(0,1);            
-  lcd2.print("world");
-  lcd2.setCursor(0,2);  
-  lcd2.print("world2");
-  lcd2.setCursor(0,3);  
-  lcd2.print("world3");
-
-  sensor.begin();
-  lcd.clear();
+ 
 }
 
 void lcd_loop(){}
--- relay.h
+++ relay.h
@@ -1,10 +1,16 @@
 #ifndef RELAY_H
 #define RELAY_H
 
+bool changeRelayState;
+
 void relay_setup()
 {
-  digitalWrite(relayPin, HIGH);
+  digitalWrite(relayPin, LOW);
   pinMode(REL, OUTPUT);
 }
-
+void relay_handler(){
+  if(changeRelayState){
+    digitalWrite(relayPin, HIGH);
+  }
+}
 #endif
