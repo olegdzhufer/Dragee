305034da503b9c26821159b903a4cdfca6d3f896
IgorIoT13 <ihor.slodziak.ir.2022lpnu.ua>
2024-06-02 19:46:01 +0300

    version 02.06

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,16 @@ void setup() {
     init_main();
     ActiveTime time;
 
+    menu.curr = STOP;
+    menu.printScreen(&menu);
+    menu.curr = Cooling;
+
 }
 
 
 
 
 void loop() {
-    main_loop();
 
 }
 
