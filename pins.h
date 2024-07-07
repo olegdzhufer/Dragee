a5ab26395d1936874ff013b39c4da50c6f8eb482
VladVanyuk <vladislavvanyuk@gmail.com>
2024-07-07 03:32:18 +0300

    Pins change

--- pins.h
+++ pins.h
@@ -1,17 +1,17 @@
 #ifndef PINS_DEF_H
 #define PINS_DEF_H
 
-#define BTN1_PIN 32
-#define BTN2_PIN 33
-#define BTN3_PIN 25
+#define BTN1_PIN 16
+#define BTN2_PIN 18
+#define BTN3_PIN 32
 
-#define LED_PIN1 19
+#define LED_PIN1 17
 #define LED_PIN2 4
 #define LED_PIN3 22
 
-#define CLK 18                     
-#define DT  5                      
-#define SW  17                       
+#define CLK 36                     
+#define DT  39                     
+#define SW  34                      
 
 #define HEAT_PIN 26
 #define COOL_PIN 27
