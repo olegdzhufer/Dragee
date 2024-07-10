6b21af94f1fdc4992f32a81d4a5065f8d7704b1a
Vladislav Vanyuk <vladislavvanyuk@gmail.com>
2024-07-10 09:21:30 +0300

    Update defs.h with debug print macros

--- mDef/defs.h
+++ mDef/defs.h
@@ -11,4 +11,13 @@
 #define OFF             false          // Set the Relay OFF
 #endif
 
+#define DEBUG 1
+
+#if defined(DEBUG) && DEBUG > 0
+ #define DEBUG_PRINT(fmt, args...) fprintf(stderr, "DEBUG: %s:%d:%s(): " fmt, \
+    __FILE__, __LINE__, __func__, ##args)
+#else
+ #define DEBUG_PRINT(fmt, args...) /* Don't do anything in release builds */
+#endif
+
 #endif
