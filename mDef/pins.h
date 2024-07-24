903bd182f11382b116925cccdf199a09182eb5c6
VladVanyuk <vladislavvanyuk@gmail.com>
2024-07-24 15:30:21 +0300

    Pins according to last PID branch version ;

--- mDef/pins.h
+++ mDef/pins.h
@@ -1,25 +1,25 @@
 #ifndef PINS_DEF_H
 #define PINS_DEF_H
 
-#define BTN1_PIN 16
+#define BTN1_PIN 32
 #define BTN2_PIN 18
-#define BTN3_PIN 32
+#define BTN3_PIN 16
 
-#define LED_PIN1 17
+#define LED_PIN1 23
 #define LED_PIN2 4
-#define LED_PIN3 22
+#define LED_PIN3 17
+
+#define HEAT_PIN 19
+#define COOL_PIN 33
+#define FAN_PIN 27
 
 #define CLK 36                     
 #define DT  39                     
 #define SW  34                      
 
-#define BUZZER_PIN 25
-
-#define HEAT_PIN 26
-#define COOL_PIN 27
-#define FAN_PIN 14
-
 #define ONE_WIRE_BUS 13
 #define ONE_WIRE_RTC 2
+
+#define BUZZER_PIN 25
 
 #endif
